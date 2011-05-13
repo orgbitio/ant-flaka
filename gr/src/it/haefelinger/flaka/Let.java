fb86e815fc61b0362b093ad5d2ce8a6ed32ae527
gilbert.rebhan <gilbert.rebhan@googlemail.com>
2011-05-13 08:14:28 +0000

    make <let> overwrite userproperties

--- gr/src/it/haefelinger/flaka/Let.java
+++ gr/src/it/haefelinger/flaka/Let.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 Haefelinger IT 
+ * Copyright (c) 2009 Haefelinger IT
  *
  * Licensed  under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -7,11 +7,11 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required  by  applicable  law  or  agreed  to in writing, 
- * software distributed under the License is distributed on an "AS 
- * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+ * Unless required  by  applicable  law  or  agreed  to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied.
- 
+
  * See the License for the specific language governing permissions
  * and limitations under the License.
  */
@@ -28,7 +28,7 @@ import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 
 /**
- * 
+ *
  * @author merzedes
  * @since 1.0
  */
