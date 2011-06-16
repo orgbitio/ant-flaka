a1b853eb05d65e166b47bb5deb4bcfb28284b237
whaefelinger <whaefelinger@gmail.com>
2011-06-16 09:42:33 +0000

    deleting reintegrated branch

--- whanno/src/it/haefelinger/flaka/el/ELFunction.java
+++
@@ -1,9 +0,0 @@
-package it.haefelinger.flaka.el;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface ELFunction {
-  String name() default "";
-}
